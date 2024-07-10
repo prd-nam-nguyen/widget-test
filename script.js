21c10f539c4902eeeb23c7aaea761adcf87e9bac
Nam Nguyen Dinh <nam.nguyen@paradox.ai>
2024-07-10 14:21:07 +0700

    hihi

---
+++ script.js
@@ -0,0 +1,12 @@
+(function(o,l) {
+    window.oliviaChatData = window.oliviaChatData || [];
+    window.oliviaChatBaseUrl = o;
+    window.oliviaChatData.push(['setKey', l]);
+    window.oliviaChatData.push(['start']);
+    var apply = document.createElement('script');
+    apply.type = 'text/javascript';
+    apply.async = true;
+    apply.src = 'https://d2cxf26suf1qae.cloudfront.net/static/site/js/widget-client.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(apply, s);
+})('https://test.paradox.ai', 'bkumrtstvhrgaimjtdhj');
