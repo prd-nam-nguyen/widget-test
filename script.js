8c6efafdb2b59a1e31fba437b44c4cd4dbdabb82
prd-nam-nguyen <162938725+prd-nam-nguyen@users.noreply.github.com>
2024-07-30 22:18:21 +0700

    test

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-// (function(o,l) {
-//     window.oliviaChatData = window.oliviaChatData || [];
-//     window.oliviaChatBaseUrl = o;
-//     window.oliviaChatData.push(['setKey', l]);
-//     window.oliviaChatData.push(['start']);
-//     var apply = document.createElement('script');
-//     apply.type = 'text/javascript';
-//     apply.async = true;
-//     apply.src = 'https://d2cxf26suf1qae.cloudfront.net/static/site/js/widget-client.js';
-//     var s = document.getElementsByTagName('script')[0];
-//     s.parentNode.insertBefore(apply, s);
-// })('https://test.paradox.ai', 'rtqiilmraytoplizfwzj');
-
 (function(o,l) {
     window.oliviaChatData = window.oliviaChatData || [];
     window.oliviaChatBaseUrl = o;
     window.oliviaChatData.push(['setKey', l]);
-    window.oliviaChatData.push(['setParent', 'my-chat-widget']);
     window.oliviaChatData.push(['start']);
     var apply = document.createElement('script');
     apply.type = 'text/javascript';
@@ -23,4 +9,18 @@
     apply.src = 'https://d2cxf26suf1qae.cloudfront.net/static/site/js/widget-client.js';
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(apply, s);
-})('https://test.paradox.ai', 'yloojrnubvjolyrpmqlj');
+})('https://test.paradox.ai', 'rtqiilmraytoplizfwzj');
+
+// (function(o,l) {
+//     window.oliviaChatData = window.oliviaChatData || [];
+//     window.oliviaChatBaseUrl = o;
+//     window.oliviaChatData.push(['setKey', l]);
+//     window.oliviaChatData.push(['setParent', 'my-chat-widget']);
+//     window.oliviaChatData.push(['start']);
+//     var apply = document.createElement('script');
+//     apply.type = 'text/javascript';
+//     apply.async = true;
+//     apply.src = 'https://d2cxf26suf1qae.cloudfront.net/static/site/js/widget-client.js';
+//     var s = document.getElementsByTagName('script')[0];
+//     s.parentNode.insertBefore(apply, s);
+// })('https://test.paradox.ai', 'yloojrnubvjolyrpmqlj');
